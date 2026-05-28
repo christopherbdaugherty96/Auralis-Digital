f49be3d4d9c11c8da48e9c250b70116c90563e5a
unknown <christopherbdaugherty@gmail.com>
2026-05-27 23:11:26 -0400

    Redirect about route to homepage section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -28,6 +28,7 @@ const App = () => (
       <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/about" element={<Navigate to="/#about" replace />} />
           <Route path="/products" element={<Index page="shop" />} />
           <Route path="/shop" element={<Index page="shop" />} />
           <Route path="/custom-design" element={<Index page="custom-design" />} />
